a8e72f653f92046e6679ac603001a3bafedabcc7
Spencer Mehta <spencermehta@gmail.com>
2022-01-24 22:20:56 +0000

    Change handling

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ const axios = require('axios');
 exports.handler = async (event) => {
   try {
     const response = await axios.get('https://pokeapi.co/api/v2/pokemon/1')
-    return JSON.stringify(response)
+    console.log(response)
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response)
+    }
   } catch (error) {
-    // return {
-    //   statusCode: 500,
-    //   body: JSON.stringify(error)
-    // }
     console.log(error);
+    return {
+      statusCode: 400,
+      body: JSON.stringify(error)
+    }
   }
 }
