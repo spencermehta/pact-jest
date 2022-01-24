1fcb52f4dba0f9a2ae3156a912aa99bbccf89add
Spencer Mehta <spencermehta@gmail.com>
2022-01-24 19:38:38 +0000

    Remove pact content and create lambda

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
-"use strict"
-
-const axios = require("axios")
-
-exports.getStations = endpoint => {
-  const url = endpoint.url
-
-  return axios
-    .request({
-      method: "GET",
-      baseURL: url,
-      url: "/stations",
-      headers: { Accept: "application/json" },
-    })
-    .then(response => response.data)
+exports.handler = async (event) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify("Hello from Lambda & GitHub!")
+  }
+  return response
 }
