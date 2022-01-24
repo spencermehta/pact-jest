fcc3cdabd72302bd84975971c928af3582c91174
Spencer Mehta <spencermehta@gmail.com>
2022-01-24 22:14:10 +0000

    Change error resp

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ exports.handler = async (event) => {
     const response = axios.get('https://pokeapi.co/api/v2/pokemon/1')
     return response
   } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify(error)
-    }
+    // return {
+    //   statusCode: 500,
+    //   body: JSON.stringify(error)
+    // }
+    console.log(error);
   }
 }
